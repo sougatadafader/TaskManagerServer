9be0c5345b062fdff0c2c9b6099ead5dcf4bfdf6
Sougata Dafader <sougatadafader@gmail.com>
2025-03-30 23:14:57 -0700

    Prepare backend for Render deployment

--- src/server/server.js
+++ src/server/server.js
@@ -87,7 +87,7 @@ app.delete("/api/tasks/:id", (req, res) => {
   });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
